dd73d486518f292287ccf99af768b482d17742a4
Verite Somwa <veritesomwa@gmail.com>
2020-04-11 04:57:19 +0200

    Added Bootstrap Template

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const app = express();
 
 let port = process.env.PORT || 3000;
 
+app.use(express.static('Public'));
+
+app.set('views', 'Views');
+app.set('view engine','hbs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.listen(port, ()=>{
     console.log(`Listening to port ${port}`);
 });
